bf99960e02cb407956ab23b546da753080914d52
Abduliitr <abdul.thomso@gmail.com>
2023-11-05 10:06:16 +0530

    day 8

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Video from './Video';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h3>REACT APP</h3>
+      <Home /> */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Home />}></Route>
+          <Route exact path='/video' element={<Video/>}></Route>
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
